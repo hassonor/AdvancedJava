609d79c924b474bad8952d8bda7e2b44a816c5ba
Or Hasson <hassonor@gmail.com>
2024-03-17 13:51:41 +0200

    make gradebook 2d int array

--- JavaHTPE11/ch_7/GradeBook/GradeBook.java
+++ JavaHTPE11/ch_7/GradeBook/GradeBook.java
@@ -2,9 +2,9 @@ package JavaHTPE11.ch_7.GradeBook;
 
 public class GradeBook {
     private String courseName;
-    private int[] grades;
+    private int[][] grades;
 
-    public GradeBook(String courseName, int[] grades) {
+    public GradeBook(String courseName, int[][] grades) {
         this.courseName = courseName;
         this.grades = grades;
     }
@@ -19,64 +19,73 @@ public class GradeBook {
 
     public void processGrades() {
         outputGrades();
-
-        System.out.printf("%nClass average is %.2f%n", getAverage());
-
         System.out.printf("Lowest grade is %d%nHighest grade is %d%n%n", getMinimum(), getMaximum());
-
         outputBarChart();
     }
 
     public int getMinimum() {
-        int lowGrade = grades[0];
+        int lowGrade = grades[0][0];
 
-        for (int grade : grades) {
-            if (grade < lowGrade) {
-                lowGrade = grade;
+        for (int[] studentGrades : grades) {
+            for (int grade : studentGrades) {
+                if (grade < lowGrade) {
+                    lowGrade = grade;
+                }
             }
         }
+
         return lowGrade;
     }
 
 
     public int getMaximum() {
-        int highGrade = grades[0];
+        int highGrade = grades[0][0];
 
-        for (int grade : grades) {
-            if (grade > highGrade) {
-                highGrade = grade;
+        for (int[] studentGrades : grades) {
+            for (int grade : studentGrades) {
+                if (grade > highGrade) {
+                    highGrade = grade;
+                }
             }
         }
         return highGrade;
     }
 
-    public double getAverage() {
+    public double getAverage(int[] setOfGrades) {
         int total = 0;
 
-        for (int grade : grades) {
+        for (int grade : setOfGrades) {
             total += grade;
         }
 
-        return (double) total / grades.length;
+        return (double) total / setOfGrades.length;
     }
 
     public void outputBarChart() {
-        System.out.println("Grade distribution: ");
+        System.out.println("Overall grade distribution:");
 
+        // stores frequency of grades in each range of 10 grades
         int[] frequency = new int[11];
 
-        for (int grade : grades) {
-            frequency[grade / 10]++;
+        // for each grade in GradeBook, increment the appropriate frequency
+        for (int[] studentGrades : grades) {
+            for (int grade : studentGrades) {
+                ++frequency[grade / 10];
+            }
         }
 
+        // for each grade frequency, print bar in chart
         for (int count = 0; count < frequency.length; count++) {
+            // output bar label ("00-09: ", ..., "90-99: ", "100: ")
             if (count == 10) {
                 System.out.printf("%5d: ", 100);
             } else {
-                System.out.printf("%02d-%02d: ", count * 10, count * 10 + 9);
+                System.out.printf("%02d-%02d: ",
+                        count * 10, count * 10 + 9);
             }
 
-            for (int starts = 0; starts < frequency[count]; starts++) {
+            // print bar of asterisks
+            for (int stars = 0; stars < frequency[count]; stars++) {
                 System.out.print("*");
             }
 
@@ -86,9 +95,27 @@ public class GradeBook {
 
     public void outputGrades() {
         System.out.printf("The grades are:%n%n");
+        System.out.print("            "); // align column heads
 
+        // create a column heading for each of the tests
+        for (int test = 0; test < grades[0].length; test++) {
+            System.out.printf("Test %d  ", test + 1);
+        }
+
+        System.out.println("Average"); // student average column heading
+
+        // create rows/columns of text representing array grades
         for (int student = 0; student < grades.length; student++) {
-            System.out.printf("Student %2d: %3d%n", student + 1, grades[student]);
+            System.out.printf("Student %2d", student + 1);
+
+            for (int test : grades[student]) { // output student's grades
+                System.out.printf("%8d", test);
+            }
+
+            // call method getAverage to calculate student's average grade;
+            // pass row of grades as the argument to getAverage
+            double average = getAverage(grades[student]);
+            System.out.printf("%9.2f%n", average);
         }
     }
 }
--- JavaHTPE11/ch_7/GradeBook/GradeBookTest.java
+++ JavaHTPE11/ch_7/GradeBook/GradeBookTest.java
@@ -2,7 +2,16 @@ package JavaHTPE11.ch_7.GradeBook;
 
 public class GradeBookTest {
     public static void main(String[] args) {
-        int[] gradesArray = {87, 68, 94, 100, 83, 78, 85, 91, 76, 87};
+        int[][] gradesArray = {{87, 96, 70},
+                {68, 87, 90},
+                {94, 100, 90},
+                {100, 81, 82},
+                {83, 65, 85},
+                {78, 87, 65},
+                {85, 75, 83},
+                {91, 94, 100},
+                {76, 72, 84},
+                {87, 93, 73}};
 
         GradeBook myGradeBook = new GradeBook(
                 "CS101 Introduction to Java Programming", gradesArray);

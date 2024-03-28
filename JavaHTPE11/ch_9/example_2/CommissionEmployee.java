c8e18b18ef4326c01ec80b719f9df66cc49f6488
Or Hasson <hassonor@gmail.com>
2024-03-28 20:22:48 +0200

    add ch_9 examples

---
+++ JavaHTPE11/ch_9/example_2/CommissionEmployee.java
@@ -0,0 +1,72 @@
+package JavaHTPE11.ch_9.example_2;
+
+public class CommissionEmployee {
+    protected final String firstName;
+    protected final String lastName;
+    protected final String socialSecurityNumber;
+    protected double grossSales; // gross weekly sales
+    protected double commissionRate; // commission percentage
+
+    public CommissionEmployee(String firstName, String lastName, String socialSecurityNumber, double grossSales, double commissionRate) {
+        if (grossSales < 0.0) {
+            throw new IllegalArgumentException("Gross sales must be >= 0.0");
+        }
+        if (commissionRate <= 0.0 || commissionRate >= 1.0) {
+            throw new IllegalArgumentException("Commission rate must be > 0.0 and < 1.0");
+        }
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.socialSecurityNumber = socialSecurityNumber;
+        this.grossSales = grossSales;
+        this.commissionRate = commissionRate;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+
+    public void setGrossSales(double grossSales) {
+        if (grossSales < 0.0) {
+            throw new IllegalArgumentException("Gross sales must be >= 0.0");
+        }
+        this.grossSales = grossSales;
+    }
+
+    public double getGrossSales() {
+        return grossSales;
+    }
+
+    public void setCommissionRate(double commissionRate) {
+        if (commissionRate <= 0.0 || commissionRate >= 1.0) {
+            throw new IllegalArgumentException("Commission rate must be > 0.0 and < 1.0");
+        }
+        this.commissionRate = commissionRate;
+    }
+
+    public double getCommissionRate() {
+        return commissionRate;
+    }
+
+    public double earnings() {
+        return commissionRate * grossSales;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s: %s %s%n%s: %s%n%s: %.2f%n%s: %.2f",
+                "commission employee", firstName, lastName,
+                "social security number", socialSecurityNumber,
+                "gross sales", grossSales,
+                "commission rate", commissionRate);
+    }
+
+}
---
+++ JavaHTPE11/ch_9/example_3/CommissionEmployee.java
@@ -0,0 +1,72 @@
+package JavaHTPE11.ch_9.example_3;
+
+public class CommissionEmployee {
+    private final String firstName;
+    private final String lastName;
+    private final String socialSecurityNumber;
+    private double grossSales; // gross weekly sales
+    private double commissionRate; // commission percentage
+
+    public CommissionEmployee(String firstName, String lastName, String socialSecurityNumber, double grossSales, double commissionRate) {
+        if (grossSales < 0.0) {
+            throw new IllegalArgumentException("Gross sales must be >= 0.0");
+        }
+        if (commissionRate <= 0.0 || commissionRate >= 1.0) {
+            throw new IllegalArgumentException("Commission rate must be > 0.0 and < 1.0");
+        }
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.socialSecurityNumber = socialSecurityNumber;
+        this.grossSales = grossSales;
+        this.commissionRate = commissionRate;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+
+    public void setGrossSales(double grossSales) {
+        if (grossSales < 0.0) {
+            throw new IllegalArgumentException("Gross sales must be >= 0.0");
+        }
+        this.grossSales = grossSales;
+    }
+
+    public double getGrossSales() {
+        return grossSales;
+    }
+
+    public void setCommissionRate(double commissionRate) {
+        if (commissionRate <= 0.0 || commissionRate >= 1.0) {
+            throw new IllegalArgumentException("Commission rate must be > 0.0 and < 1.0");
+        }
+        this.commissionRate = commissionRate;
+    }
+
+    public double getCommissionRate() {
+        return commissionRate;
+    }
+
+    public double earnings() {
+        return getCommissionRate() * getGrossSales();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s: %s %s%n%s: %s%n%s: %.2f%n%s: %.2f",
+                "commission employee", getFirstName(), getLastName(),
+                "social security number", getSocialSecurityNumber(),
+                "gross sales", getGrossSales(),
+                "commission rate", getCommissionRate());
+    }
+
+}

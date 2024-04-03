92dfcc87f6dd8bceea1cdf8caf8361adb3b14a59
Or Hasson <hassonor@gmail.com>
2024-04-03 15:08:03 +0300

    adding divide by zero without no exception handling demo

---
+++ JavaHTPE11/ch_11_exception_handling/divideByZeroWithoutException/DivideByZeroNoExceptionHandling.java
@@ -0,0 +1,22 @@
+package JavaHTPE11.ch_11_exception_handling.divideByZeroWithoutException;
+
+import java.util.Scanner;
+
+public class DivideByZeroNoExceptionHandling {
+    public static int quotient(int numerator, int denominator) {
+        return numerator / denominator; // possible division by zero
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter an integer numerator: ");
+        int numerator = scanner.nextInt();
+        System.out.print("Please enter a denominator: ");
+        int denominator = scanner.nextInt();
+
+        int result = quotient(numerator, denominator);
+        System.out.println("Result: " + numerator + " / " + denominator + " = " + result);
+    }
+
+}

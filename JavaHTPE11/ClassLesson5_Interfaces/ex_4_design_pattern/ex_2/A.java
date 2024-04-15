ab9fb34b227e78ecb82c8341c9d3d9177b7f1b48
Or Hasson <hassonor@gmail.com>
2024-04-15 20:02:52 +0300

    add more exmaples for ex_2 design pattern

---
+++ JavaHTPE11/ClassLesson5_Interfaces/ex_4_design_pattern/ex_2/A.java
@@ -0,0 +1,19 @@
+package JavaHTPE11.ClassLesson5_Interfaces.ex_4_design_pattern.ex_2;
+
+import java.util.Iterator;
+
+public class A implements Iterator {
+
+    @Override
+    public boolean hasNext() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public Object next() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
---
+++ JavaHTPE11/ClassLesson5_Interfaces/ex_4_design_pattern/ex_2/B.java
@@ -0,0 +1,13 @@
+package JavaHTPE11.ClassLesson5_Interfaces.ex_4_design_pattern.ex_2;
+
+import java.util.Iterator;
+
+public class B implements Iterable {
+
+    @Override
+    public Iterator iterator() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}

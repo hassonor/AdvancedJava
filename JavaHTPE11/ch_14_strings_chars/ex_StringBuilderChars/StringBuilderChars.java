220aba2e6d7924541795c4b8be557d2a656b224c
Or Hasson <hassonor@gmail.com>
2024-04-21 17:52:07 +0300

    adding stringBuilder chars demo

---
+++ JavaHTPE11/ch_14_strings_chars/ex_StringBuilderChars/StringBuilderChars.java
@@ -0,0 +1,25 @@
+package JavaHTPE11.ch_14_strings_chars.ex_StringBuilderChars;
+
+public class StringBuilderChars {
+    public static void main(String[] args) {
+        StringBuilder buffer = new StringBuilder("hello there");
+
+        System.out.println("buffer = " + buffer.toString());
+        System.out.println("Character at 0: " + buffer.charAt(0) + "\nCharacter at 4: " + buffer.charAt(4));
+
+        char[] charArray = new char[buffer.length()];
+        buffer.getChars(0, buffer.length(), charArray, 0);
+        System.out.print("The characters are: ");
+        for (char character : charArray) {
+            System.out.print(character);
+        }
+        System.out.println();
+
+        buffer.setCharAt(0, 'H');
+        buffer.setCharAt(6, 'T');
+        System.out.println("buffer = " + buffer.toString());
+
+        buffer.reverse();
+        System.out.println("buffer = " + buffer.toString());
+    }
+}

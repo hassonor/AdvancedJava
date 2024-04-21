0c26eea6cf64b8d73f81b19b47a550f2afe5f9dc
Or Hasson <hassonor@gmail.com>
2024-04-21 11:26:51 +0300

    adding stringIndexMethods example

---
+++ JavaHTPE11/ch_14_strings_chars/ex_5_string_index_methods/StringIndexMethods.java
@@ -0,0 +1,27 @@
+package JavaHTPE11.ch_14_strings_chars.ex_5_string_index_methods;
+
+public class StringIndexMethods {
+    public static void main(String[] args) {
+        String letters = "abcdefghijklmabcdefghijklm";
+
+        // test indexOf to locate a character in a string
+        System.out.println("'c' is located at index " + letters.indexOf('c'));
+        System.out.println("'a' is located at index " + letters.indexOf('a', 1));
+        System.out.println("'$' is located at index " + letters.indexOf('$'));
+
+        // test lastIndexOf to find a character in a string
+        System.out.println("Last 'c' is located at index " + letters.lastIndexOf('c'));
+        System.out.println("Last 'a' is located at index " + letters.lastIndexOf('a', 25));
+        System.out.println("Last '$' is located at index " + letters.lastIndexOf('$'));
+
+        // test indexOf to locate a substring in a string
+        System.out.println("\"def\" is located at index " + letters.indexOf("def"));
+        System.out.println("\"def\" is located at index " + letters.indexOf("def", 7));
+        System.out.println("\"def\" is located at index " + letters.indexOf("hello"));
+
+        // test lastIndexOf to find a substring in a string
+        System.out.println("Last \"def\" is located at index " + letters.lastIndexOf("def"));
+        System.out.println("Last \"def\" is located at index " + letters.lastIndexOf("def", 25));
+        System.out.println("Last \"hello\" is located at index " + letters.lastIndexOf("hello"));
+    }
+}

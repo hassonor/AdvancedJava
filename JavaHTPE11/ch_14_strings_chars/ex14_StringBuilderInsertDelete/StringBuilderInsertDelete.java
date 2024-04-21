aeea3211ad7186c242d4f4cc76d836c78c883b55
Or Hasson <hassonor@gmail.com>
2024-04-21 18:22:27 +0300

    insert,deleteCharAt and delete demos

---
+++ JavaHTPE11/ch_14_strings_chars/ex14_StringBuilderInsertDelete/StringBuilderInsertDelete.java
@@ -0,0 +1,43 @@
+package JavaHTPE11.ch_14_strings_chars.ex14_StringBuilderInsertDelete;
+
+public class StringBuilderInsertDelete {
+    public static void main(String[] args) {
+        Object objectRef = "hello";
+        String string = "goodbye";
+        char[] charArray = {'a', 'b', 'c', 'd', 'e', 'f'};
+        boolean booleanValue = true;
+        char characterValue = 'K';
+        int integerValue = 7;
+        long longValue = 10000000;
+        float floatValue = 2.5f; // suffix indicates that 2.5 is a float
+        double doubleValue = 33.333;
+
+        StringBuilder buffer = new StringBuilder();
+        buffer.insert(0, objectRef);
+        buffer.insert(0, "  ");
+        buffer.insert(0, string);
+        buffer.insert(0, "  ");
+        buffer.insert(0, charArray);
+        buffer.insert(0, "  ");
+        buffer.insert(0, charArray, 3, 3);
+        buffer.insert(0, "  ");
+        buffer.insert(0, booleanValue);
+        buffer.insert(0, "  ");
+        buffer.insert(0, characterValue);
+        buffer.insert(0, "  ");
+        buffer.insert(0, integerValue);
+        buffer.insert(0, "  ");
+        buffer.insert(0, longValue);
+        buffer.insert(0, "  ");
+        buffer.insert(0, floatValue);
+        buffer.insert(0, "  ");
+        buffer.insert(0, doubleValue);
+
+        System.out.println("buffer after inserts: " + buffer.toString());
+
+        buffer.deleteCharAt(10); // deleted 5 in 2.5
+        buffer.delete(2, 6); // delete .333 in 33.333
+
+        System.out.println("buffer after deletes: " + buffer.toString());
+    }
+}

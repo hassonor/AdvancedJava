fa8e78fe3b4b38b549557446f7a5cc630d64caf0
Or Hasson <hassonor@gmail.com>
2024-04-29 14:29:37 +0300

    add Sort2 example reverse order collections

---
+++ JavaHTPE11/ch_16_generic_collections/ex4_collections_methods/Sort2.java
@@ -0,0 +1,18 @@
+package JavaHTPE11.ch_16_generic_collections.ex4_collections_methods;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Sort2 {
+    public static void main(String[] args) {
+        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+
+        // Create and display a list containing the suits array elements
+        List<String> list = Arrays.asList(suits);
+        System.out.printf("Unsorted array elements: %s%n", list);
+
+        Collections.sort(list, Collections.reverseOrder()); // sort ArrayList
+        System.out.printf("Sorted array elements: %s%n", list);
+    }
+}

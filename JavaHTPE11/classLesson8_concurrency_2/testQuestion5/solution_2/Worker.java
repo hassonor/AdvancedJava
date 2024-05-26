6a13c1d70eb85fec995ea2231f0c07d5bcc1ee3c
Or Hasson <hassonor@gmail.com>
2024-05-26 15:01:24 +0300

    testQuestion5 - 2 ways to solve

---
+++ JavaHTPE11/classLesson8_concurrency_2/testQuestion5/solution_2/Worker.java
@@ -0,0 +1,20 @@
+package JavaHTPE11.classLesson8_concurrency_2.testQuestion5.solution_2;
+
+import JavaHTPE11.classLesson8_concurrency_2.testQuestion5.solution_1.Work;
+
+public class Worker extends Thread {
+    private int d;
+    private Work w;
+
+    public Worker(int d, Work w) {
+        this.d = d;
+        this.w = w;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        w.stage1(d);
+        w.stage2(d);
+    }
+}

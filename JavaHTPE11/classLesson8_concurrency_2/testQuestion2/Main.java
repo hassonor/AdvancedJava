978bd70756c84be963f785d85df05f7de849665c
Or Hasson <hassonor@gmail.com>
2024-05-26 12:19:16 +0300

    testQuestion2

---
+++ JavaHTPE11/classLesson8_concurrency_2/testQuestion2/Main.java
@@ -0,0 +1,18 @@
+package JavaHTPE11.classLesson8_concurrency_2.testQuestion2;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] mat = new int[4][4];
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                mat[i][j] = i + j;
+            }
+        }
+        Manager m = new Manager(mat);
+        Worker w1 = new Worker(m);
+        Worker w2 = new Worker(m);
+        w1.start();
+        w2.start();
+        System.out.print(m.getTotal());
+    }
+}
---
+++ JavaHTPE11/classLesson8_concurrency_2/testQuestion2/Worker.java
@@ -0,0 +1,27 @@
+package JavaHTPE11.classLesson8_concurrency_2.testQuestion2;
+
+public class Worker extends Thread {
+
+    private Manager m;
+
+    public Worker(Manager m) {
+        this.m = m;
+    }
+
+
+    @Override
+    public void run() {
+        super.run();
+        int[] a = m.allocateRow();
+        while (a != null) {
+            int sumOfRow = 0;
+            for (int item : a) {
+                sumOfRow += item;
+            }
+            m.addSum(sumOfRow);
+            a = m.allocateRow();
+        }
+        m.getTotal();
+
+    }
+}

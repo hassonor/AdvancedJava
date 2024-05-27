80f33bef5daabcf70ca33ffdc6e756fe72e7a055
Or Hasson <hassonor@gmail.com>
2024-05-27 18:56:47 +0300

    parrallelism with lock and unlock

---
+++ JavaHTPE11/ClassLesson7_concurrency/review/example_5_parrallelism_lock/CashRegister.java
@@ -0,0 +1,30 @@
+package JavaHTPE11.ClassLesson7_concurrency.review.example_5_parrallelism_lock;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class CashRegister {
+    private int sum = 0;
+
+    private Lock lock = new ReentrantLock();
+
+
+    public void add(int toAdd) {
+        lock.lock();
+        try {
+            sum += toAdd;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public int get() {
+        lock.lock();
+        try {
+            return sum;
+        } finally {
+            lock.unlock();
+        }
+
+    }
+}

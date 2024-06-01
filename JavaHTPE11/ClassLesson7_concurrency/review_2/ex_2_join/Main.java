5740d3c0d6a0f9a955c5b026b50953c58d1970f5
Or Hasson <hassonor@gmail.com>
2024-06-01 16:27:29 +0300

    join example

---
+++ JavaHTPE11/ClassLesson7_concurrency/review_2/ex_2_join/Main.java
@@ -0,0 +1,23 @@
+package JavaHTPE11.ClassLesson7_concurrency.review_2.ex_2_join;
+
+public class Main {
+    public static void main(String[] args) {
+        OneToTwenty one = new OneToTwenty("ONE");
+        OneToTwenty two = new OneToTwenty("TWO");
+        OneToTwenty three = new OneToTwenty("THREE");
+
+        one.start();
+        two.start();
+        three.start();
+
+        try {
+            one.join();
+            two.join();
+            three.join();
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+        
+        System.out.println("MAIN DONE");
+    }
+}
---
+++ JavaHTPE11/ClassLesson7_concurrency/review_2/ex_2_join/OneToTwenty.java
@@ -0,0 +1,18 @@
+package JavaHTPE11.ClassLesson7_concurrency.review_2.ex_2_join;
+
+public class OneToTwenty extends Thread {
+    private final int MAX = 20;
+    private String name;
+
+    public OneToTwenty(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        for (int i = 0; i < MAX; i++) {
+            System.out.println("Thread " + name + " :" + i);
+        }
+    }
+}

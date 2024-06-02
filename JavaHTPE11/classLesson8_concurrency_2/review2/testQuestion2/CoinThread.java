2c99b95e24ae7eeff97764054e26ecde9558e534
Or Hasson <hassonor@gmail.com>
2024-06-02 11:08:32 +0300

    review 2 testQuest2

---
+++ JavaHTPE11/classLesson8_concurrency_2/review2/testQuestion2/CoinThread.java
@@ -0,0 +1,29 @@
+package JavaHTPE11.classLesson8_concurrency_2.review2.testQuestion2;
+
+public class CoinThread extends Thread {
+    private int id;
+
+    private Monitor m;
+
+    public CoinThread(int id, Monitor m) {
+        this.id = id;
+        this.m = m;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        int head = 0, tail = 0;
+
+        for (int i = 0; i < 100; i++) {
+            if ((Math.random() < 0.5)) {
+                head++;
+            } else {
+                tail++;
+            }
+        }
+        m.waitForMyPrint(id);
+        System.out.println("thread " + id + " heads = " + head + " tails = " + tail);
+        m.threadDone();
+    }
+}
---
+++ JavaHTPE11/classLesson8_concurrency_2/review2/testQuestion2/Monitor.java
@@ -0,0 +1,37 @@
+package JavaHTPE11.classLesson8_concurrency_2.review2.testQuestion2;
+
+public class Monitor {
+    private int turn = 1;
+
+    int num;
+
+    public Monitor(int num) {
+        this.num = num;
+    }
+
+
+    public synchronized void waitForMyPrint(int num) {
+        while (num >= turn) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public synchronized void threadDone() {
+        turn++;
+        notifyAll();
+    }
+
+    public synchronized void waitForAllDone() {
+        while (turn <= num) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}

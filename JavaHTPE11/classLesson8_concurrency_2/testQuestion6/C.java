20bad5d6b646e7235954b6fd0435702f3849a2a8
Or Hasson <hassonor@gmail.com>
2024-06-04 18:03:10 +0300

    Add more testQuestion (tricky)

---
+++ JavaHTPE11/classLesson8_concurrency_2/testQuestion6/C.java
@@ -0,0 +1,25 @@
+package JavaHTPE11.classLesson8_concurrency_2.testQuestion6;
+
+public class C {
+    private int i;
+    private static int j;
+
+    void print1(){
+        System.out.println(i + ", " + j);
+    }
+
+    void print2(){
+        System.out.println(i + "," + j);
+    }
+
+    public static void main(String[] args){
+        int k;
+        C x = new C();
+        // System.out.println(i); // Compilation Error!
+        System.out.println(j);
+       //  System.out.println(k); // Compilation Error!
+        System.out.println(x.i);
+        System.out.println(x.j);
+
+    }
+}
---
+++ JavaHTPE11/classLesson8_concurrency_2/testQuestion7/FF.java
@@ -0,0 +1,13 @@
+package JavaHTPE11.classLesson8_concurrency_2.testQuestion7;
+
+public class FF {
+    public void foo(){
+        System.out.println("foo");
+        bar();
+    }
+
+    private void bar(){
+        System.out.println("bar 1");
+    }
+
+}

a9d8b949f747b90cde8e6be8726ab3a7481803d8
Or Hasson <hassonor@gmail.com>
2024-06-06 12:20:26 +0300

    add testQuestion generic static method isSorted

---
+++ JavaHTPE11/classLesson10/testQuestion_generics/IllegalDataException.java
@@ -0,0 +1,11 @@
+package JavaHTPE11.classLesson10.testQuestion_generics;
+
+public class IllegalDataException extends Exception{
+    public IllegalDataException(){
+        super();
+    }
+
+    public IllegalDataException(String n){
+        super(n);
+    }
+}

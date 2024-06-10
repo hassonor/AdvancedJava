6bde1d8fdeb44400cbbcf8babec293a7ebad6205
Or Hasson <hassonor@gmail.com>
2024-06-10 20:16:56 +0300

    add example9 basic use of join

---
+++ JavaHTPE11/ClassLesson8_concurrency_2/V2/example8_basic_use_join/TryThread.java
@@ -0,0 +1,34 @@
+package JavaHTPE11.ClassLesson8_concurrency_2.V2.example8_basic_use_join;
+
+class TryThread extends Thread {
+    public TryThread(String firstName, String secondName, long delay) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        aWhile = delay;
+        //setDaemon(true);
+    }
+
+    public void run() {
+        try {
+            while (total < 1000) {
+                System.out.print(firstName);
+                sleep(aWhile);
+                total += aWhile;
+                System.out.print(secondName + "\n");
+            }
+            System.out.print(secondName + " stoped.\n");
+        } catch (InterruptedException e) {
+            System.out.println(firstName + secondName + e);
+        }
+    }
+
+    private String firstName;
+    private String secondName;
+    private long aWhile;
+    private long total;
+
+    public long stamGetTotal() {
+
+        return total;
+    }
+}

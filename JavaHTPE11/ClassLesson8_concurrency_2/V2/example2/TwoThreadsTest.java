85aa4e49fbda97e17481f5973658deb8b46082b4
Or Hasson <hassonor@gmail.com>
2024-06-10 19:09:41 +0300

    Example 2 v2

---
+++ JavaHTPE11/ClassLesson8_concurrency_2/V2/example2/TwoThreadsTest.java
@@ -0,0 +1,86 @@
+package JavaHTPE11.ClassLesson8_concurrency_2.V2.example2;
+
+public class TwoThreadsTest {
+    public static void main(String[] args) {
+        new SimpleThread("Jamaica").start();
+        new SimpleThread("Fiji").start();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+        System.out.println("MainMainMainMainMainMainMain==================================Main! ");
+
+    }
+}
+

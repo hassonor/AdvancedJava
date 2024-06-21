770a1a351b97b06a069a552ff95eb484178e0347
Or Hasson <hassonor@gmail.com>
2024-06-21 16:32:42 +0300

    test

--- JavaHTPE11/ClassLesson11_test_questions/testQuestion4_oop/Main.java
+++ JavaHTPE11/ClassLesson11_test_questions/testQuestion4_oop/Main.java
@@ -3,6 +3,7 @@ package JavaHTPE11.ClassLesson11_test_questions.testQuestion4_oop;
 public class Main {
     public static void main(String[] args) {
         func(new Z(), new Z());
+        funA(4);
     }
 
     public static void func(X a, Y b) {
@@ -15,6 +16,10 @@ public class Main {
 
     public static void func(Y a, Y b) {
         System.out.println("third");
+    }
+
+    public static <T> void funA(T x) {
+
     }
 
     // If we remove the third method the code won't compile

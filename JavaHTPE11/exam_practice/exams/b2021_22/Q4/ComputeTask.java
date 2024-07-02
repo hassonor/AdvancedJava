13705b0d33f94df1da80ab2c347c7bef0c71e0ab
Or Hasson <hassonor@gmail.com>
2024-07-02 14:54:09 +0300

    improve b2022_22 Q4 sum method

--- JavaHTPE11/exam_practice/exams/b2021_22/Q4/ComputeTask.java
+++ JavaHTPE11/exam_practice/exams/b2021_22/Q4/ComputeTask.java
@@ -1,38 +1,35 @@
 package JavaHTPE11.exam_practice.exams.b2021_22.Q4;
 
-public class ComputeTask extends Thread{
+public class ComputeTask extends Thread {
     private int[] arr;
     private boolean isFinished = false;
     private int sum;
 
 
-    public ComputeTask(int[] arr){
+    public ComputeTask(int[] arr) {
         this.arr = arr;
     }
 
     @Override
-    public void run(){
-        int sum = 0;
-        for(int i =0;i < arr.length;i++)
-            sum+=arr[i];
+    public void run() {
+        sum();
+    }
 
-        synchronized(this){
-            sum(sum);
-            isFinished = true;
-            notifyAll();
+    public synchronized void sum() {
+        int localSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            localSum += arr[i];
         }
+        this.sum = localSum;
+        isFinished = true;
+        notifyAll();
     }
 
-    public synchronized void sum(int sum){
-        this.sum +=sum;
-    }
-
-    public synchronized int getSum(){
-        while(!isFinished){
-            try{
+    public synchronized int getSum() {
+        while (!isFinished) {
+            try {
                 wait();
-            }
-            catch(InterruptedException e){
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }

9695c958a2694b73e4827863959fcbc0afd65dd6
Or Hasson <hassonor@gmail.com>
2024-07-03 12:33:09 +0300

    a2021_74 Q1 abstract class

---
+++ JavaHTPE11/exam_practice/exams/a2021_74/Q1/X.java
@@ -0,0 +1,19 @@
+package JavaHTPE11.exam_practice.exams.a2021_74.Q1;
+
+public abstract class X {
+
+    public X() {
+    }
+
+    public X(X x) {
+
+    }
+
+    public abstract void Y();
+
+    public void Z() {
+        Y();
+    }
+
+    ;
+}

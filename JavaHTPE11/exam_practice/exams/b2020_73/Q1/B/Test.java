150ef256bcc606434362a4330ff0d0f4896e5f27
Or Hasson <hassonor@gmail.com>
2024-07-06 16:57:37 +0300

    b2020_73 Q1-B code

---
+++ JavaHTPE11/exam_practice/exams/b2020_73/Q1/B/Test.java
@@ -0,0 +1,8 @@
+package JavaHTPE11.exam_practice.exams.b2020_73.Q1.B;
+
+public class Test {
+    public static void main(String[] args) {
+        X x = new X();
+        Y y = (Y) x;
+    }
+}
---
+++ JavaHTPE11/exam_practice/exams/b2020_73/Q1/B/Y.java
@@ -0,0 +1,4 @@
+package JavaHTPE11.exam_practice.exams.b2020_73.Q1.B;
+
+public class Y {
+}

d33e691190623ec36bd316e38b75cff244567c99
Or Hasson <hassonor@gmail.com>
2024-07-06 15:54:14 +0300

    a2020_82 Q4 solve

---
+++ JavaHTPE11/exam_practice/exams/a2020_82/Q4/Test.java
@@ -0,0 +1,17 @@
+package JavaHTPE11.exam_practice.exams.a2020_82.Q4;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] vec = {1, 2, 3, 4};
+        WorkThread[] workers = new WorkThread[3];
+
+        for (int i = 0; i < 3; i++) {
+            workers[i] = new WorkThread(vec, i);
+        }
+        for (int i = 0; i < 3; i++) {
+            workers[i].start();
+        }
+
+        System.out.println("**main**");
+    }
+}

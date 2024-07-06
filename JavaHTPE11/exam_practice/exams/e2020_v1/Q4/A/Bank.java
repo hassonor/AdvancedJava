6309e205c6e3c943c47cf4603e8fabbb78feb029
Or Hasson <hassonor@gmail.com>
2024-07-06 11:19:40 +0300

    e2020 Q4-A solve

---
+++ JavaHTPE11/exam_practice/exams/e2020_v1/Q4/A/Bank.java
@@ -0,0 +1,31 @@
+package JavaHTPE11.exam_practice.exams.e2020_v1.Q4.A;
+
+public class Bank {
+    private double[] accounts;
+
+    public Bank(int size, double initialBalance) {
+        accounts = new double[size];
+        for (int i = 0; i < size; i++) {
+            accounts[i] = initialBalance;
+        }
+    }
+
+    public synchronized boolean transfer(int from, int to, double amount) {
+        while (accounts[from] < amount) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        accounts[from] = accounts[from] - amount;
+        accounts[to] = accounts[to] + amount;
+        notifyAll();
+        return true;
+    }
+
+    public int size() {
+        return accounts.length;
+    }
+
+}

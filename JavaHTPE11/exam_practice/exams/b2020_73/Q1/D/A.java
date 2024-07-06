ff35dbc811bf35e50055ab7a7f027b067d342800
Or Hasson <hassonor@gmail.com>
2024-07-06 17:17:08 +0300

    b2020_73 Q1-D code

---
+++ JavaHTPE11/exam_practice/exams/b2020_73/Q1/D/A.java
@@ -0,0 +1,12 @@
+package JavaHTPE11.exam_practice.exams.b2020_73.Q1.D;
+
+public class A {
+    private int a = 3;
+    public int b = 4;
+
+    private void foo() {
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
+
---
+++ JavaHTPE11/exam_practice/exams/b2020_73/Q1/D/B.java
@@ -0,0 +1,17 @@
+package JavaHTPE11.exam_practice.exams.b2020_73.Q1.D;
+
+
+public class B {
+    public static void main(String[] args) {
+        A aa = new A() {
+            private int a = 2;
+
+            public void foo() {
+                System.out.println(a);
+                System.out.println(b);
+            }
+        };
+
+    }
+
+}

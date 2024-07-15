e398b941caf3cd4b303a39e58fb48c5c96ca06f4
Or Hasson <hassonor@gmail.com>
2024-07-15 18:53:04 +0300

    a2024_94 Q3-B

---
+++ JavaHTPE11/exam_practice/exams/a2024_94/Q3/B/Test.java
@@ -0,0 +1,9 @@
+package JavaHTPE11.exam_practice.exams.a2024_94.Q3.B;
+
+public class Test {
+    public static void main(String[] args) {
+        MyList<B> list1 = new MyList<B>();
+        list1.add(new B());
+//        list1.add(new C()); // ! Compilation Error
+    }
+}
